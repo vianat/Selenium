4b70d3752da11fdf7b746493a7fc42e0cec8203b
Sergei Nikiforov <vianatdev@gmail.com>
2024-05-29 19:04:10 -0700

    e2e

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -33,9 +33,9 @@ public class Main {
 
         driver.findElement(By.xpath("//input[@id='billing_first_name']")).sendKeys("Fufel");
         driver.findElement(By.xpath("//input[@id='billing_last_name']")).sendKeys("Fufelov");
-        driver.findElement(By.xpath("//input[@id='billing_city']")).sendKeys("Fufel town");
+        driver.findElement(By.xpath("//input[@id='billing_city']")).sendKeys("Fufelburg");
         driver.findElement(By.xpath("//input[@id='billing_postcode']")).sendKeys("47373");
-        driver.findElement(By.xpath("//input[@id='billing_email']")).sendKeys("47373@gmail.com");
+        driver.findElement(By.xpath("//input[@id='billing_email']")).sendKeys("Fufel@gmail.com");
 
         Thread.sleep(1500);
 
@@ -44,6 +44,55 @@ public class Main {
         Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(),
                 "Checkout");
 
-        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public  void e2eWithLogin() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://askomdch.com");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.cssSelector("#menu-item-1227 a")).click();
+
+        driver.findElement(By.cssSelector("#woocommerce-product-search-field-0")).sendKeys("blue");
+
+        driver.findElement(By.cssSelector("button[value='Search']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//h1[contains(text(),'Search results: “blue”')]")).getText(),
+                "Search results: “blue”");
+
+        driver.findElement(By.cssSelector("a[aria-label='Add “Blue Shoes” to your cart']")).click();
+
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("a[title='View cart']")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//a[normalize-space()='Blue Shoes']")).getText(),
+                "Blue Shoes");
+
+        driver.findElement(By.xpath("//a[normalize-space()='Proceed to checkout']")).click();
+
+        driver.findElement(By.cssSelector(".showlogin")).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("username")).sendKeys("fufel");
+        driver.findElement(By.id("password")).sendKeys("TsqvCmNjwGde2tm");
+        driver.findElement(By.cssSelector("button[value='Login']")).click();
+
+        driver.findElement(By.xpath("//input[@id='billing_first_name']")).sendKeys("Fufel");
+        driver.findElement(By.xpath("//input[@id='billing_last_name']")).sendKeys("Fufelov");
+        driver.findElement(By.xpath("//input[@id='billing_city']")).sendKeys("Fufelburg");
+        driver.findElement(By.xpath("//input[@id='billing_postcode']")).sendKeys("47373");
+        driver.findElement(By.xpath("//input[@id='billing_email']")).sendKeys("Fufel@gmail.com");
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("place_order")).click();
+        Thread.sleep(1000);
+
+        Assert.assertEquals(driver.findElement(By.tagName("h1")).getText(),
+                "Checkout");
+
+        driver.quit();
     }
 }
