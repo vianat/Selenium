d0dd353b94fdc853e7d45f0a52fa5b896e6624e3
Sergei Nikiforov <vianatdev@gmail.com>
2024-06-04 09:30:27 -0700

    pom

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -48,50 +48,43 @@ public class MainTest extends BaseTest {
 
     }
 
-//    @Test
-//    public  void e2eWithLogin() throws InterruptedException {
-//
-//        BillingAddress billingAddress = new BillingAddress();
-//        billingAddress.setFirstName("myName");
-//        billingAddress.setLastName("myLastName");
-//        billingAddress.setCity("myCity");
-//        billingAddress.setZip("47373");
-//        billingAddress.setEmail("thisisme@gmail.com");
-//
-//        HomePage hp = new HomePage(driver);
-//
-//        StorePage sp = hp.clickStoreMenuLink();
-//
-//        sp.enterTextInSearchField("blue");
-//        sp.clickSearchBtn();
-//
-//        Assert.assertEquals(sp.getTitle(),"Search results: “blue”");
-//
-//        sp.clickAddToCartBtn("Blue Shoes");
-//
-//        Thread.sleep(1500);
-//
-//        CartPage cartPage = sp.clickViewCart();
-//
-//        Assert.assertEquals(cartPage.getProductName(), "Blue Shoes");
-//
-//        CheckoutPage checkoutPage = cartPage.clickCheckoutBtn();
-//
-//        driver.findElement(checkoutPage.showLogin).click();
-//        Thread.sleep(1000);
-//
-//        checkoutPage.login("Fufel","TsqvCmNjwGde2tm");
-//
-//        checkoutPage.clickCheckoutBtn();
-//        checkoutPage.setBillingAddress(billingAddress);
-//        checkoutPage.clickPlaceOrder();
-//
-//        Thread.sleep(1000);
-//
-//        checkoutPage.clickPlaceOrder();
-//        Thread.sleep(1000);
-//
-//        Assert.assertEquals(checkoutPage.getNoticeText(), "Checkout");
-//
-//    }
+    @Test
+    public  void e2eWithLogin() throws InterruptedException {
+
+        BillingAddress billingAddress = new BillingAddress();
+        billingAddress.setFirstName("myName");
+        billingAddress.setLastName("myLastName");
+        billingAddress.setCity("myCity");
+        billingAddress.setZip("47373");
+        billingAddress.setEmail("thisisme@gmail.com");
+
+        HomePage hp = new HomePage(driver);
+
+        StorePage sp = hp.clickStoreMenuLink();
+
+        sp.enterTextInSearchField("blue");
+        sp.clickSearchBtn();
+
+        Assert.assertEquals(sp.getTitle(),"Search results: “blue”");
+
+        sp.clickAddToCartBtn("Blue Shoes");
+
+        Thread.sleep(1500);
+
+        CartPage cartPage = sp.clickViewCart();
+
+        Assert.assertEquals(cartPage.getProductName(), "Blue Shoes");
+
+        CheckoutPage checkoutPage = cartPage.clickCheckoutBtn();
+
+        driver.findElement(checkoutPage.showLogin).click();
+        Thread.sleep(1000);
+
+        checkoutPage.login("Fufel","TsqvCmNjwGde2tm");
+
+        checkoutPage.setBillingAddress(billingAddress).placeOrder();
+
+        Assert.assertEquals(checkoutPage.getNoticeText(), "Checkout");
+
+    }
 }
