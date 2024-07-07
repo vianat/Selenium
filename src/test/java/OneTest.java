993de4fddb7fd27d4546809d5cc59bae5c564785
Sergei Nikiforov <vianatdev@gmail.com>
2024-07-07 11:05:38 -0700

    fix xml

---
+++ src/test/java/OneTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+public class OneTest {
+    @Test
+    public void testMethod() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/tests/OneTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class OneTest {
+    @Test
+    public void testMethod() {
+        assertTrue(true);
+    }
+}
